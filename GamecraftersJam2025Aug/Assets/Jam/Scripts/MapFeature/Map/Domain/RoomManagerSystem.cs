cc472c4b02f6a04a35727ed8e194c81fb6059190
Madgine <marinabelogaz@gmail.com>
2025-09-04 20:58:25 +0300

    added chest mock;

--- GamecraftersJam2025Aug/Assets/Jam/Scripts/MapFeature/Map/Domain/RoomManagerSystem.cs
+++ GamecraftersJam2025Aug/Assets/Jam/Scripts/MapFeature/Map/Domain/RoomManagerSystem.cs
@@ -1,5 +1,6 @@
 using System;
 using Jam.Scripts.Gameplay;
+using Jam.Scripts.Gameplay.ChestReward;
 using Jam.Scripts.MapFeature.Map.Data;
 using Zenject;
 
@@ -9,6 +10,7 @@ namespace Jam.Scripts.MapFeature.Map.Domain
     {
         [Inject] private BattleStarter _battleStarter;
         [Inject] private RoomEventSystem _eventSystem;
+        [Inject] private ChestRewardSystem _chestRewardSystem;
 
         public void ChooseRoomToOpen(Room room){
             switch (room.Type) {
@@ -17,14 +19,8 @@ namespace Jam.Scripts.MapFeature.Map.Domain
                 case RoomType.EliteFight:
                     _battleStarter.StartBattle(room);
                     break;
-                case RoomType.Merchant:
-                    //tbd 
-                    break;
                 case RoomType.Chest:
-                    //todo idk
-                    break;
-                case RoomType.Campfire:
-                    //tbd 
+                    _chestRewardSystem.Handle(room);
                     break;
                 case RoomType.Event:
                     _eventSystem.StartEvent(room);
