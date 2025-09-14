20e4837e4dfc7d08b3c8d077d2adea16acaf3029
RomanT <r.tarasyuta@chillgaming.net>
2025-09-15 04:39:48 +0700

    lose ui invoke

--- GamecraftersJam2025Aug/Assets/Jam/Scripts/Gameplay/Rooms/Battle/Battle/Result/BattleLosePresenter.cs
+++ GamecraftersJam2025Aug/Assets/Jam/Scripts/Gameplay/Rooms/Battle/Battle/Result/BattleLosePresenter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Jam.Scripts.Gameplay.Battle.Player;
 using Jam.Scripts.MapFeature.Map.Domain;
 using Jam.Scripts.SceneManagement;
 using Zenject;
@@ -8,6 +9,7 @@ namespace Jam.Scripts.Gameplay.Rooms.Battle
     public class BattleLosePresenter : IInitializable, IDisposable
     {
         [Inject] private BattleEventBus _battleEventBus;
+        [Inject] private PlayerEventBus _playerBus;
         [Inject] private MapEventBus _mapEventBus;
         [Inject] private BattleLoseUi _loseUi;
         [Inject] private SceneChanger _sceneChanger;
@@ -16,12 +18,14 @@ namespace Jam.Scripts.Gameplay.Rooms.Battle
         {
             _battleEventBus.OnLose += ShowLoseScreen;
             _loseUi.GameOverButton.onClick.AddListener(FinishGame);
+            _playerBus.OnDeath += ShowLoseScreen;
         }
 
         public void Dispose()
         {
             _battleEventBus.OnLose += ShowLoseScreen;
             _loseUi.GameOverButton.onClick.RemoveListener(FinishGame);
+            _playerBus.OnDeath -= ShowLoseScreen;
         }
 
         private void FinishGame()
