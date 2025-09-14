b1d689e37673a1ad33e58eb8b522125e34522a8e
ccor3xx <161947661+ccor3xx@users.noreply.github.com>
2025-09-14 20:51:43 +0300

    Artifacts conf upgrade

--- GamecraftersJam2025Aug/Assets/Jam/Scripts/Gameplay/Rooms/Battle/Enemy/Model/EnemyType.cs
+++ GamecraftersJam2025Aug/Assets/Jam/Scripts/Gameplay/Rooms/Battle/Enemy/Model/EnemyType.cs
@@ -3,12 +3,18 @@
     public enum EnemyType
     {
         None = 0,
-        RatEasy = 10,
-        RatMedium = 11,
-        RatHard = 12,
-        Frog = 20,
-        Monkey = 30,
-        FlyingHead = 40,
+        RatTier1 = 10,
+        RatTier2 = 11,
+        MonkeyTier1 = 13,
+        MonkeyTier2 = 14,
+		MonkeyTier3 = 15,
+		PuppetTier1 = 16,
+		PuppetTier1_1 = 17,
+		PuppetTier2 = 18,
+		PuppetTier3 = 19,
+		ClownServ1 = 20,
+		ClownServ2 = 21,
+		RatBoss = 30,
         
     }
 }
